a38efd5cd455e744f1ab66c402d7c986cba6412b
Piranha20103 <webphantom20103@gmail.com>
2022-08-01 23:32:25 +0900

    fix: update loading effect

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
+//@import components
 import Socials from "components/Socials";
-import { Outlet } from "react-router-dom";
 //@import layouts
 import Header from "./header";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
 
 const Layout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    nprogress.start();
+    nprogress.done();
+  }, [location.pathname]);
+
   return (
     <>
       <Header />
